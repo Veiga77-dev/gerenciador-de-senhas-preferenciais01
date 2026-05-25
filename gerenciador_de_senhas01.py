903ba2a132d20de5236d80989d15d5239e3af8e6
gransr <aqworldsbot001@gmail.com>
2026-05-25 08:00:54 -0300

    Update gerenciador_de_senhas01.py

--- gerenciador_de_senhas01.py
+++ gerenciador_de_senhas01.py
@@ -12,3 +12,17 @@ total_chamados = 0
 cont_preferencial = 0
 cont_urgente = 0
 cont_normal = 0
+
+tipo_escolhido = "NORMAL"
+
+COR_FUNDO     = "#0d1117"
+COR_TOPO      = "#161b22"
+COR_CARD      = "#21262d"
+COR_CARD2     = "#162032"
+COR_AZUL      = "#58a6ff"
+COR_VERDE     = "#3fb950"
+COR_VERMELHO  = "#f85149"
+COR_CIANO     = "#79c0ff"
+COR_BRANCO    = "#e6edf3"
+COR_CINZA     = "#8b949e"
+COR_BORDA     = "#30363d"
