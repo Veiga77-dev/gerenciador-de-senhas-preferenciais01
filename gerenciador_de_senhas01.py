32aac10de79c1c91aaa7a6b150ee7338d3352df4
JoãoPedro.Veiga77 <joaopedroveigasilva23@gmail.com>
2026-06-01 16:09:10 +0000

    Adiciona cor amarelo

--- gerenciador_de_senhas01.py
+++ gerenciador_de_senhas01.py
@@ -15,17 +15,18 @@ cont_normal = 0
 
 tipo_escolhido = "NORMAL"
 
-COR_FUNDO     = "#0d1117"
-COR_TOPO      = "#161b22"
-COR_CARD      = "#21262d"
-COR_CARD2     = "#162032"
-COR_AZUL      = "#58a6ff"
-COR_VERDE     = "#3fb950"
-COR_VERMELHO  = "#f85149"
-COR_CIANO     = "#79c0ff"
-COR_BRANCO    = "#e6edf3"
-COR_CINZA     = "#8b949e"
-COR_BORDA     = "#30363d"
+COR_FUNDO = "#0d1117"
+COR_TOPO = "#161b22"
+COR_CARD = "#21262d"
+COR_CARD2 = "#162032"
+COR_AZUL = "#58a6ff"
+COR_VERDE = "#3fb950"
+COR_AMARELO = "#d29922"
+COR_VERMELHO = "#f85149"
+COR_CIANO = "#79c0ff"
+COR_BRANCO = "#e6edf3"
+COR_CINZA = "#8b949e"
+COR_BORDA = "#30363d"
 
 servicos = [
     "Atendimento Geral",
